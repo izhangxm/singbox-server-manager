ccfcfb08abe7659ff2eac88709a04b2cb84951da
izhagnxm <root@certain-text-1.localdomain>
2023-03-11 23:23:50 +0800

    update

--- websubsribe.py
+++ websubsribe.py
@@ -18,11 +18,8 @@ service_state_cmd = "systemctl status sing-box"
 service_restart_cmd = "systemctl restart sing-box"
 service_stop_cmd = "systemctl stop sing-box"
 
-# 远程调用的rpc密钥
-rpc_key = "rpckey1235"
 
-# args = Namespace(dst_server_cfg="/etc/sing-box/config.json", tp_server_cfg="./data/tp_server_cfg.json", users="./data/users.json", subscribe_tp="./data/clash_tp.yaml")
-args = Namespace(dst_server_cfg="./runtime/server_tmp_config.json", server_info="./data/server_info.yaml", subscribe_tp="./data/clash_tp.yaml")
+server_info_file = "./profile.yaml"
 
 
 class ServiceState():
@@ -33,7 +30,7 @@ class ServiceState():
 
 def load_server_info():
     # server_info = json.load(open(args.server_info, 'r'))
-    server_info = yaml.load(open(args.server_info, 'r'))
+    server_info = yaml.load(open(server_info_file, 'r'))
     return server_info
 
 
@@ -42,12 +39,13 @@ def get_users_from_db():
 
 
 def get_subscribe_tp():
-    subscribe_tp = yaml.load(open(args.subscribe_tp, 'r'))
+    serverinfo = load_server_info()
+    subscribe_tp = yaml.load(open(serverinfo['subscribe_tp'], 'r'))
     return subscribe_tp
 
-def dump_server_config(server_cfg):
+def dump_server_config(server_cfg, dst_server_cfg):
     
-    json.dump(server_cfg, open(args.dst_server_cfg, 'w+'),
+    json.dump(server_cfg, open(dst_server_cfg, 'w+'),
               sort_keys=False, indent=2, separators=(',', ':'))
     # yaml.dump(subscribe_tp, open(args.dst_server_cfg,'w+'))
 
@@ -148,6 +146,9 @@ def api_update_server():
         
         # dns 相关
         server_cfg_res['dns'] = server_info['dns']
+        
+        # log 相关
+        server_cfg_res['log'] = server_info['log']
                 
         
         # 生成配置文件的inbounds
@@ -155,12 +156,15 @@ def api_update_server():
         interfaces = server_info['inbounds']['interfaces']
         for tag, interface in interfaces.items():
             
+            # 当前监听服务的描述信息
             meta = interface["meta"]
             
+            # 是否通过shadowtls来通信
             if meta['over_shadowtls']:
                 s_p = meta['tls_s_port']
+                # 为保证兼容性 生成三个版本的配置
+                # v1-v3的tls配置
                 v1_shadow_tls = deepcopy(shadowtls_tp)
-
                 v1_shadow_tls.update({"listen_port": s_p,"version": 1,"detour": tag})
                 
                 v2_shadow_tls = deepcopy(shadowtls_tp)
@@ -172,17 +176,29 @@ def api_update_server():
                 
                 inbounds_res += [v1_shadow_tls,v2_shadow_tls,v3_shadow_tls]
                 
+                # 加密类型配置
                 content = interface['content']
                 content.update(listen_common)
                 content.update({"listen":"127.0.0.1","tag":tag})
 
+                # 是否支持多用户
                 if meta['support_multiuser']:
-                    content.update({"users":users_dt})
+                    uname_key = meta.get("uname_key")
+                    users = deepcopy(users_dt)
+                    if uname_key:
+                        for user in users:
+                            _uname = user.pop('name')
+                            user[uname_key] = _uname
+                    content.update({"users":users})
+                else:
+                    # TODO
+                    pass
                 
                 inbounds_res += [content]
                 
+        
         server_cfg_res['inbounds'] = inbounds_res
-        dump_server_config(server_cfg_res)
+        dump_server_config(server_cfg_res, server_info["dst_server_cfg"])
 
     except Exception as e:
         resp_data['code'] = 0
@@ -200,6 +216,7 @@ if __name__ == "__main__":
     # get_service_state()
     # print(service_op("status"))
     api_update_server()
+    print(service_op("restart"))
 
     # a = get_random_password()
     # print(a)
